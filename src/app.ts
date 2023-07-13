9d941254bceaeeffe326035fadb7e3b6c4e343f4
alongw <alongw@alongw.cn>
2023-07-13 17:17:42 +0800

    feat: improve the function of loading plug-ins

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,20 @@ const app = express()
 
 // 解析JSON
 app.use(bodyParser.json())
+
 // 解决跨域问题
 app.use(cors())
+
+// 加载路由插件
+config.enable_plugins.forEach(async (e) => {
+    const { default: plugin, Name: pluginName } = await import(`./plugins/${e}`)
+    try {
+        logger.info(`尝试加载插件 ${pluginName || '未知插件'}(${e})`)
+        app.use(`/${e}`, plugin)
+    } catch (error) {
+        logger.warn(`加载插件 ${pluginName || '未知插件'}(${e}) 失败，因为插件不符合规范`)
+    }
+})
 
 // 监听端口
 app.listen(config.listen_port, () => {
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,4 @@
 export interface TypeConfig {
     listen_port: number
+    enable_plugins: string[]
 }
