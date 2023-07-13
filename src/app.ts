c56eecd84af2ceb6a16956a2caf5e7be89412470
alongw <alongw@alongw.cn>
2023-07-14 02:05:32 +0800

    feat: pay-code plugin

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,14 @@ app.use(bodyParser.json())
 // 解决跨域问题
 app.use(cors())
 
+// 用户访问日志
+app.use('/', (req, res, next) => {
+    logger.info(
+        `用户[${req.ip}] 尝试${req.method}请求 ${req.path} 用户代理：${req.headers['user-agent']}`
+    )
+    next()
+})
+
 // 加载路由插件
 config.enable_plugins.forEach(async (e) => {
     const { default: plugin, Name: pluginName } = await import(`./plugins/${e}`)
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,13 @@
 export interface TypeConfig {
     listen_port: number
     enable_plugins: string[]
+    plugins_config: {
+        'pay-code': {
+            default: 'wechat' | 'alipay' | 'qq' | 'wechatpay'
+            alipay: null | string
+            wechat: null | string
+            qq: null | string
+            wechatpay: null | string
+        }
+    }
 }
