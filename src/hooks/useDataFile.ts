7774939bc63888b388a7dfc62d7f8414a166431b
alongw <alongw@alongw.cn>
2023-10-28 03:08:53 +0800

    feat: useDataFile hook

---
+++ src/hooks/useDataFile.ts
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import logger from './../utils/log'
+
+export const useDataFile = (pluginsName: string) => {
+    // 获取文件
+    const getFile = (file: string, defaultFile?: string) => {
+        const path = `./data/${pluginsName}${file.at(0) === '/' ? file : `/${file}`}`
+        // 检测文件是否存在
+        try {
+            fs.readFileSync(path)
+        } catch {
+            // 创建该文件
+            fs.writeFileSync(path, defaultFile || '', 'utf-8')
+        }
+        // 读取
+        return fs.readFileSync(path, 'utf-8') as string
+    }
+
+    // 覆写文件
+    const writeFile = (file: string, value: string) => {
+        try {
+            fs.writeFileSync(
+                `./data/${pluginsName}${file.at(0) === '/' ? file : `/${file}`}`,
+                value,
+                'utf-8'
+            )
+            return true
+        } catch (error) {
+            logger.error(`文件写入失败！(${error.message}) ${error}`)
+        }
+        return false
+    }
+
+    return {
+        getFile,
+        writeFile
+    }
+}
