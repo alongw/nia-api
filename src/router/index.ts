a477662a7c7e675aa67b6c4d637ad86eaf7ea982
alongw <alongw@alongw.cn>
2023-12-03 01:13:24 +0800

    feat: remove plugins docs

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,8 +9,4 @@ router.use('/', async (req, res, next) =>
     (await import('./base/index')).default(req, res, next)
 )
 
-router.use('/nia', async (req, res, next) =>
-    (await import('./nia/index')).default(req, res, next)
-)
-
 export default router
